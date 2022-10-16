3643741c83bdba11edfd7fc6858013b4d697c7e7
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-16 12:47:55 -0400

    Initial set up of the stock detail and overview pages

---
+++ src/pages/StockDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StockDetailPage() {
+  return (
+    <div>This is the stock detail page</div>
+  )
+}
+
+export default StockDetailPage
---
+++ src/pages/StockOverviewPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StockOverviewPage() {
+  return (
+    <div>This is the Stock Overview Page</div>
+  )
+}
+
+export default StockOverviewPage
