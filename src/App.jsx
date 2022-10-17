155ec867ead54b0a233dd75471e602b7ccbbe9c0
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-17 15:00:25 -0400

    Work on routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StockOverviewPage } from './pages/StockOverviewPage'
+import { StockDetailPage } from './pages/StockDetailPage'
 
 function App() {
   return (
-    <div className="container">
-      Stock Trading App
-    </div>
+    <main>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StockOverviewPage />}></Route>
+        <Route path="/detail/:symbol" element={<StockDetailPage />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
