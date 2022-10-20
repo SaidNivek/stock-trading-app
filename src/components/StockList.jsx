020967925f81d4e3e19f6f634f54d504ac6917be
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-20 06:51:21 -0400

    Install Axios

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -3,6 +3,8 @@ import {  useState, useEffect } from 'react'
 export const StockList = () => {
     // Will hold the names of the stocks that someone wants to watch and to set those stocks
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
+
+    
     
     return (
         <div>
