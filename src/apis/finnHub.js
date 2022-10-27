8665d1ee50303d52cf61c77fdd6ff00515b27c44
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-27 13:26:36 -0400

    Work on the fetch requests and setStocks useState

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
+const TOKEN = process.env.REACT_APP_FINNHUB_KEY
 
 // This export will automatically include the basue URL for the finnHub API, so we don't have to keep coding that in to everything we do
 export default axios.create({
-    baseURL: 'https://finnhub.io/api/v1'
+    baseURL: 'https://finnhub.io/api/v1',
+    // The params for token will be included with the finnHub axios instance, so we don't have to include it in the fetch request used wherever in the program that we call it
+    params: {
+        token: TOKEN,
+    }
 })
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -3,6 +3,7 @@ import finnHub from '../apis/finnHub'
 
 export const StockList = () => {
     
+    const [stock, setStock] = useState()
     // Will hold the names of the stocks that someone wants to watch and to set those stocks
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
     
@@ -10,18 +11,44 @@ export const StockList = () => {
     const API_KEY = process.env.REACT_APP_FINNHUB_KEY
 
     useEffect(() => {
+        let isMounted = true
         // Must be an async function so it can work properly with the await
         const fetchData = async () => {
             try {
+                const responses = []
                 // Uses the finnHub axios import to start with the base URL route to make it easier to read/use
-                // Add the API_KEY from the .env folder
-                const response = await finnHub.get("/quote?symbol=MSFT&token=" + API_KEY)
-                console.log(response)
+                // The token after the symbol isn't needed, since we passed it as a aparm in the finnHub axios instance (see apis/finnHub.js)
+                // After the /quote, the object that is passed in has params, which are needed by the fetch request to allow it to match the API docs
+                const response1 = await finnHub.get("/quote?", {
+                    params: {
+                        symbol: "GOOGL"
+                    }
+                })
+                responses.push(response1)
+                const response2 = await finnHub.get("/quote?", {
+                    params: {
+                        symbol: "MSFT"
+                    }
+                })
+                responses.push(response2)
+                const response3 = await finnHub.get("/quote?", {
+                    params: {
+                        symbol: "AMZN"
+                    }
+                })
+                responses.push(response3)
+                console.log(responses)
+                // This will check if the component is mounted. If it is, then we will setStock to the data, if not, then it will skip this and move on
+                if (isMounted) {
+                    // Will take the response.data and put it into the stock variable
+                    setStock(responses)
+                }
             } catch (err) {
                 console.log(err)
             }
         }
         fetchData()
+        return () => (isMounted = false)
     }, [])
     
     return (
