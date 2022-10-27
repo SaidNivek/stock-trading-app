1228baf8352f9e741185249048bc11d2901cb478
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-27 10:12:01 -0400

    Add comments

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -6,13 +6,15 @@ export const StockList = () => {
     // Will hold the names of the stocks that someone wants to watch and to set those stocks
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
     
+    // Import the key from the .env folder to be used more easily
     const API_KEY = process.env.REACT_APP_FINNHUB_KEY
 
-    console.log(API_KEY)
-
     useEffect(() => {
+        // Must be an async function so it can work properly with the await
         const fetchData = async () => {
             try {
+                // Uses the finnHub axios import to start with the base URL route to make it easier to read/use
+                // Add the API_KEY from the .env folder
                 const response = await finnHub.get("/quote?symbol=MSFT&token=" + API_KEY)
                 console.log(response)
             } catch (err) {
