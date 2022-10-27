52eaacf027bd9b98dffdda83c00b04634f482bad
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-27 10:10:25 -0400

    Axios fetch works for dummy data

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,15 +1,19 @@
 import {  useState, useEffect } from 'react'
 import finnHub from '../apis/finnHub'
-import API_KEY from '../.gitignore'
 
 export const StockList = () => {
+    
     // Will hold the names of the stocks that someone wants to watch and to set those stocks
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
+    
+    const API_KEY = process.env.REACT_APP_FINNHUB_KEY
+
+    console.log(API_KEY)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await finnHub.get("/quote?symbol=MSFT&token=cd5mla2ad3i5nc8o2vagcd5mla2ad3i5nc8o2vb0")
+                const response = await finnHub.get("/quote?symbol=MSFT&token=" + API_KEY)
                 console.log(response)
             } catch (err) {
                 console.log(err)
