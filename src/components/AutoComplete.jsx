bb0b7524dcc657ef5f9f297f47ade1601e50bd71
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-01 17:18:14 -0400

    Fix typo

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -4,8 +4,14 @@ export const AutoComplete = () => {
     return (
         <div className="w-50 p-5 rounded mx-auto">
             <div className="form-floating dropdown">
-                <input style={{backgroundColor: "rgba(145,158,171, 0.04)"}} type="text"  id="search"
-                classname="form-control" placeholder="Search" />
+                <input style={{backgroundColor: "rgba(145,158,171, 0.04)"}} type="text" id="search"
+                className="form-control" placeholder="Search" autoComplete="off"></input>
+                <label htmlFor="search">Search</label>
+                <ul className="dropdown-menu">
+                    <li>stock 1</li>
+                    <li>stock 2</li>
+                    <li>stock 3</li>
+                </ul>
             </div>
         </div>
     )
