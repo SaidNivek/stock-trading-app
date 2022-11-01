508314710593c21e06dec70884c77f69cdf4cc32
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-01 16:56:46 -0400

    Fixing bootstrap issues

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,5 +1,12 @@
+
+
 export const AutoComplete = () => {
     return (
-        <div>AutoComplete</div>
+        <div className="w-50 p-5 rounded mx-auto">
+            <div className="form-floating dropdown">
+                <input style={{backgroundColor: "rgba(145,158,171, 0.04)"}} type="text"  id="search"
+                classname="form-control" placeholder="Search" />
+            </div>
+        </div>
     )
 }
