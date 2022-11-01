9a3aa6ee67a31176ca4437e625bd5a431827ceb3
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-01 12:43:31 -0400

    Fix loading issue with ternary in StockList.jsx and render data to screen

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -3,9 +3,9 @@ import finnHub from '../apis/finnHub'
 
 export const StockList = () => {
     
-    const [stock, setStock] = useState()
+    const [stock, setStock] = useState([])
     // Will hold the names of the stocks that someone wants to watch and to set those stocks
-    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
+    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
     
     useEffect(() => {
         let isMounted = true
@@ -49,10 +49,42 @@ export const StockList = () => {
         console.log(stock)
         return () => (isMounted = false)
     }, [])
-    
-    return (
-        <div>
-            Stock List Page
-        </div>
+
+    return (   
+    stock === [] ?  <h5>Loading...</h5> 
+        : (
+            <div>
+                <table className="table hover mt-5">
+                    <thead style={{color: "rgb(78,89,102)"}}>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Chg</th>
+                            <th scope="col">Chg %</th>
+                            <th scope="col">High</th>
+                            <th scope="col">Low</th>
+                            <th scope="col">Open</th>
+                            <th scope="col">Close</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stock.map((stockData) => {
+                            return (
+                                <tr className="table-row" key={stockData.symbol}>
+                                    <th scope="row">{stockData.symbol}</th>
+                                    <td>{stockData.data.c}</td>
+                                    <td>{stockData.data.d}</td>
+                                    <td>{stockData.data.dp}</td>
+                                    <td>{stockData.data.h}</td>
+                                    <td>{stockData.data.l}</td>
+                                    <td>{stockData.data.o}</td>
+                                    <td>{stockData.data.pc}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
     )
 }
