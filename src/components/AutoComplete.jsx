015987d745ea705f823417c68e84708a6dc724f6
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-02 13:43:03 -0400

    Add state for the search function and set the value to update

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,11 +1,12 @@
-
+import { useState } from 'react'
 
 export const AutoComplete = () => {
+    const [search, setSearch] = useState("")
     return (
         <div className="w-50 p-5 rounded mx-auto">
             <div className="form-floating dropdown">
                 <input style={{backgroundColor: "rgba(145,158,171, 0.04)"}} type="text" id="search"
-                className="form-control" placeholder="Search" autoComplete="off"></input>
+                className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
                 <label htmlFor="search">Search</label>
                 <ul className="dropdown-menu">
                     <li>stock 1</li>
