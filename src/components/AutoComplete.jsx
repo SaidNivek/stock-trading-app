6fd10236e495ff529ae6c2748cb19d0ae41f642b
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-02 15:25:15 -0400

    Work on fetchData for the AutoComplete search function

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from 'react'
 import finnHub from "../apis/finnHub"
 
 export const AutoComplete = () => {
+    // This state is used to set the search bar to the value of the search, and update it using setSearch right from the input field (below)
     const [search, setSearch] = useState("")
+    const [results, setResults] = useState([])
 
+    // This effect will request from the API data that is being searched for, auto-completing the search input
     useEffect(() => {
+        let isMounted = true
         const fecthData = async () => {
             try {
                 const response = await finnHub.get("/search", {
@@ -12,13 +16,26 @@ export const AutoComplete = () => {
                         q: search
                     }
                 })
-                console.log(response)
+                if (isMounted) {
+
+                    setResults(response.data.result)
+                }
+
             } catch (error) {
                 console.log(error)
             }
         }
-        fecthData()
-    }, [])
+        // If the user has put in at least one letter, then we call fetchData
+        if(search.length > 0) {
+            fecthData()
+        // If the user deletes down to 0 characters, setResults to an empty array
+        } else {
+            setResults([])
+        }
+
+        return () => (isMounted = false)
+
+    }, [search])
 
     return (
         <div className="w-50 p-5 rounded mx-auto">
