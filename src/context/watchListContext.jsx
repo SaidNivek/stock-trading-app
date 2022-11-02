13e499f08464084711fc29c27227d0614f8ffcf2
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-02 17:05:17 -0400

    Create addStock and removeStock functions

--- src/context/watchListContext.jsx
+++ src/context/watchListContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const WatchListContext = createContext()
 
@@ -7,7 +7,25 @@ export const WatchListContextProvider = (props) => {
     // Will hold the names of the stocks that someone wants to watch and to set those stocks
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
 
-    return <WatchListContext.Provider value={{ watchList }}>
+    // This function will add a stock to the watchList of stocks, if it is not in the list already
+    const addStock = (stock) => {
+        // If the stock that is selected is not currently in the list, add it in
+        // indexOf returns -1 if the queried item is NOT in the array
+        if(watchList.indexOf(stock) === -1) {
+            setWatchList([...watchList, stock])
+        }
+    }
+
+    // This function will remove a stock from the watchList of stocks, if it is in the list, otherwise it does nothing
+    const deleteStock = (stock) => {
+        // Filter will go through and return all the elements that DO NOT equal the selected stock
+        // If it does match, it will not be returned back to the array, which is reset using setWatchList
+        setWatchList(watchList.filter((el) => {
+            return el !== stock
+        }))
+    }
+
+    return <WatchListContext.Provider value={{ watchList, addStock, deleteStock }}>
         {props.children}
     </WatchListContext.Provider>
 }
