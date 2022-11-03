322324730086790eed122c38aa91a0c0cce50772
SaidNivek <kevin.vi.dias@gmail.com>
2022-11-03 14:39:53 -0400

    Render one day data of a stock using Apex Charts

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -1,7 +1,42 @@
-import ApexCharts from 'apexcharts'
+import Chart from "react-apexcharts";
 
-export const StockChart = () => {
+// Call in the props that are passed into chart data and destructure them instead of using props
+export const StockChart = ({chartData, symbol}) => {
     // Deconstruct the chart data that is passed into the component 
     const { day, week, year } = chartData
-    return <h3>Stock Chart</h3>
+    const options = {
+        title: {
+            text: symbol,
+            align: "center",
+            style: {
+                fontSize: "24px"
+            }            
+        },
+        chart: {
+            id: "stock data",
+            speed: 1300
+        },
+        xaxis: {
+            type: 'datetime',
+            labels: {
+                datetimeUTC: false
+            }
+        }
+    }
+
+    const series = [{
+        name: symbol,
+        data: day
+    }]
+
+    return (
+        <div className="mt-5 p-4 shadow-sm bg-white">
+            <Chart 
+                options={options}
+                series={series}
+                type="area"
+                width="100%"
+            />
+        </div>
+    )
 }
